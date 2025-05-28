f06ae979af001451f6c86620e1bba265e38235e5
Yan Muller <yan.muller.contact@gmail.com>
2025-05-27 21:00:59 -0300

    etapa II complemento

--- Etapa-II/src/PrincipalCandidato.java
+++ Etapa-II/src/PrincipalCandidato.java
@@ -1,17 +1,17 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class PrincipalCandidato {
     public static void main(String[] args) {
         Random ran = new Random();
+        Scanner scan = new Scanner(System.in);
 
         String[] names = {"Antonio", "Roberto", "Gabriela","Leonardo", "Eunice", "Ana", "Ariel"};
         String[] parties = {"CCCP", "PP", "AL","DJA","AK"};
 
         Candidato[] candidatos = new Candidato[ran.nextInt(100)+1];
-        System.out.println("*************************TAMANHO "+candidatos.length);
-        
 
         for(int i=0; i<candidatos.length;i++){
             String randomName = names[ran.nextInt(names.length)];
@@ -28,7 +28,18 @@ public class PrincipalCandidato {
         for (Candidato candidato : candidatos) {
             System.out.println(candidato);
         }
+
+        System.out.println("Procure um candidato");
+        String entrada = scan.nextLine();
+        int resultado = pesquisaBinariaCandidatos(candidatos, entrada);
+        if (resultado != -1) {
+            System.out.println("Candidato encontrado no índice: " + resultado);
+            System.out.println("Detalhes: " + candidatos[resultado]);
+        }else {
+            System.out.println("Candidato não encontrado.");
+        }
     }
+
     public static void ordenaCandidatoPorNome(Candidato[] candidatos){
         for (int i = 1; i < candidatos.length; i++) {
             Candidato chave = candidatos[i];
@@ -67,5 +78,26 @@ public class PrincipalCandidato {
             }
             candidatos[j + 1] = chave;
         }
+    }
+
+    public static int pesquisaBinariaCandidatos(Candidato[] candidatos, String nomeProcurado) {
+        int inicio = 0, fim = candidatos.length - 1;
+
+        while (inicio <= fim) {
+            int meio = (inicio + fim) / 2;
+            String nomeCandidato = candidatos[meio].getFirstName(); 
+
+            int comparacao = nomeCandidato.compareToIgnoreCase(nomeProcurado);
+
+            if (comparacao == 0) {
+                return meio;
+            } else if (comparacao < 0) {
+                inicio = meio + 1;
+            } else {
+                fim = meio - 1;
+            }
+        }
+
+        return -1;
     }
 }
